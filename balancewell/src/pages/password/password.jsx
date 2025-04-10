044fa8d194ca9b7f4c97ed31d9ae9a3868efd004
yiru075 <yliu0577@student.monash.edu>
2025-04-10 17:28:49 +1000

    Update password.jsx

--- balancewell/src/pages/password/password.jsx
+++ balancewell/src/pages/password/password.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Password.css';
+import './password.css';
 
 const Password = () => {
   const [username, setUsername] = useState('');
