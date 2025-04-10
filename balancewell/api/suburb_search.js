464d8ae042ed59ba8e2d72493824985acaf116ce
yiru075 <yliu0577@student.monash.edu>
2025-04-10 18:57:43 +1000

    Add API

--- balancewell/api/suburb_search.js
+++ balancewell/api/suburb_search.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const query = req.query.q;
   const sessionToken = req.query.session_token || 'default_token';
 
