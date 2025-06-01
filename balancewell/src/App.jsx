65a6ff1a8c4f27b94d53211823a6b3bafd7f6062
yiru075 <yliu0577@student.monash.edu>
2025-06-01 13:31:33 +1000

    delete password page

--- balancewell/src/App.jsx
+++ balancewell/src/App.jsx
@@ -15,8 +15,6 @@ import Sustainability from './pages/sustainability/sustainability.jsx';
 import News from './pages/news/news.jsx';
 import Quiz from './pages/quiz/quiz.jsx';
 import TopicPageWrapper from './components/TopicNavigation/TopicPageWrapper.jsx';
-import Password from './pages/password/password.jsx';
-import ProtectedRoute from './components/projectedRoute.jsx';
 import Events from './pages/events/events.jsx';
 import Calculation from './pages/Calculation/Calculation';
 import RetirementSupport from './pages/retirementSupport/retirementSupport.jsx';
@@ -65,7 +63,7 @@ function App() {
 
 function AppContent() {
   const location = useLocation();
-  const showHeader = location.pathname !== '/'; // Hide header on the password page
+  const showHeader = true;
 
   const wrapTopicComponent = (Component) => {
     return (
@@ -79,48 +77,48 @@ function AppContent() {
     <>
       {showHeader && <Header />}
       <Routes>
-        <Route path="/" element={<Password />} />
-        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/sustainability" element={<ProtectedRoute><Sustainability /></ProtectedRoute>} />
-        <Route path="/housing" element={<ProtectedRoute><Housing /></ProtectedRoute>} />
-        <Route path="/events" element={<ProtectedRoute><Events /></ProtectedRoute>} />
-        <Route path="/retirementSupport" element={<ProtectedRoute><RetirementSupport /></ProtectedRoute>} />
-        <Route path="/advisors" element={<ProtectedRoute><Advisors /></ProtectedRoute>} />
-        <Route path="/education" element={<ProtectedRoute><Education /></ProtectedRoute>} />
-        <Route path="/eduMenu" element={<ProtectedRoute><EduMenu /></ProtectedRoute>} />
-        <Route path="/news" element={<ProtectedRoute><News /></ProtectedRoute>} />
-        <Route path="/quiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/sustainability" element={<Sustainability />} />
+        <Route path="/housing" element={<Housing />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/retirementSupport" element={<RetirementSupport />} />
+        <Route path="/advisors" element={<Advisors />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/eduMenu" element={<EduMenu />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/quiz" element={<Quiz />} />
         <Route path="/calculation" element={<Calculation />} />
 
         {/* Level 1 */}
-        <Route path="/level1/Topic1_1" element={<ProtectedRoute>{wrapTopicComponent(Topic1_1)}</ProtectedRoute>} />
-        <Route path="/level1/Topic1_2" element={<ProtectedRoute>{wrapTopicComponent(Topic1_2)}</ProtectedRoute>} />
-        <Route path="/level1/Topic1_3" element={<ProtectedRoute>{wrapTopicComponent(Topic1_3)}</ProtectedRoute>} />
-        <Route path="/level1/Topic1_4" element={<ProtectedRoute>{wrapTopicComponent(Topic1_4)}</ProtectedRoute>} />
-        <Route path="/level1/Topic1_5" element={<ProtectedRoute>{wrapTopicComponent(Topic1_5)}</ProtectedRoute>} />
-        <Route path="/level1/Topic1_6" element={<ProtectedRoute>{wrapTopicComponent(Topic1_6)}</ProtectedRoute>} />
-        <Route path="/level1/Topic1_7" element={<ProtectedRoute>{wrapTopicComponent(Topic1_7)}</ProtectedRoute>} />
-        <Route path="/level1/Topic1_8" element={<ProtectedRoute>{wrapTopicComponent(Topic1_8)}</ProtectedRoute>} />
+        <Route path="/level1/Topic1_1" element={wrapTopicComponent(Topic1_1)} />
+        <Route path="/level1/Topic1_2" element={wrapTopicComponent(Topic1_2)} />
+        <Route path="/level1/Topic1_3" element={wrapTopicComponent(Topic1_3)} />
+        <Route path="/level1/Topic1_4" element={wrapTopicComponent(Topic1_4)} />
+        <Route path="/level1/Topic1_5" element={wrapTopicComponent(Topic1_5)} />
+        <Route path="/level1/Topic1_6" element={wrapTopicComponent(Topic1_6)} />
+        <Route path="/level1/Topic1_7" element={wrapTopicComponent(Topic1_7)} />
+        <Route path="/level1/Topic1_8" element={wrapTopicComponent(Topic1_8)} />
 
         {/* Level 2 */}
-        <Route path="/level2/Topic2_1" element={<ProtectedRoute>{wrapTopicComponent(Topic2_1)}</ProtectedRoute>} />
-        <Route path="/level2/Topic2_2" element={<ProtectedRoute>{wrapTopicComponent(Topic2_2)}</ProtectedRoute>} />
-        <Route path="/level2/Topic2_3" element={<ProtectedRoute>{wrapTopicComponent(Topic2_3)}</ProtectedRoute>} />
-        <Route path="/level2/Topic2_4" element={<ProtectedRoute>{wrapTopicComponent(Topic2_4)}</ProtectedRoute>} />
-        <Route path="/level2/Topic2_5" element={<ProtectedRoute>{wrapTopicComponent(Topic2_5)}</ProtectedRoute>} />
-        <Route path="/level2/Topic2_6" element={<ProtectedRoute>{wrapTopicComponent(Topic2_6)}</ProtectedRoute>} />
-        <Route path="/level2/Topic2_7" element={<ProtectedRoute>{wrapTopicComponent(Topic2_7)}</ProtectedRoute>} />
-        <Route path="/level2/Topic2_8" element={<ProtectedRoute>{wrapTopicComponent(Topic2_8)}</ProtectedRoute>} />
+        <Route path="/level2/Topic2_1" element={wrapTopicComponent(Topic2_1)} />
+        <Route path="/level2/Topic2_2" element={wrapTopicComponent(Topic2_2)} />
+        <Route path="/level2/Topic2_3" element={wrapTopicComponent(Topic2_3)} />
+        <Route path="/level2/Topic2_4" element={wrapTopicComponent(Topic2_4)} />
+        <Route path="/level2/Topic2_5" element={wrapTopicComponent(Topic2_5)} />
+        <Route path="/level2/Topic2_6" element={wrapTopicComponent(Topic2_6)} />
+        <Route path="/level2/Topic2_7" element={wrapTopicComponent(Topic2_7)} />
+        <Route path="/level2/Topic2_8" element={wrapTopicComponent(Topic2_8)} />
 
         {/* Level 3 */}
-        <Route path="/level3/Topic3_1" element={<ProtectedRoute>{wrapTopicComponent(Topic3_1)}</ProtectedRoute>} />
-        <Route path="/level3/Topic3_2" element={<ProtectedRoute>{wrapTopicComponent(Topic3_2)}</ProtectedRoute>} />
-        <Route path="/level3/Topic3_3" element={<ProtectedRoute>{wrapTopicComponent(Topic3_3)}</ProtectedRoute>} />
-        <Route path="/level3/Topic3_4" element={<ProtectedRoute>{wrapTopicComponent(Topic3_4)}</ProtectedRoute>} />
-        <Route path="/level3/Topic3_5" element={<ProtectedRoute>{wrapTopicComponent(Topic3_5)}</ProtectedRoute>} />
-        <Route path="/level3/Topic3_6" element={<ProtectedRoute>{wrapTopicComponent(Topic3_6)}</ProtectedRoute>} />
-        <Route path="/level3/Topic3_7" element={<ProtectedRoute>{wrapTopicComponent(Topic3_7)}</ProtectedRoute>} />
-        <Route path="/level3/Topic3_8" element={<ProtectedRoute>{wrapTopicComponent(Topic3_8)}</ProtectedRoute>} />
+        <Route path="/level3/Topic3_1" element={wrapTopicComponent(Topic3_1)} />
+        <Route path="/level3/Topic3_2" element={wrapTopicComponent(Topic3_2)} />
+        <Route path="/level3/Topic3_3" element={wrapTopicComponent(Topic3_3)} />
+        <Route path="/level3/Topic3_4" element={wrapTopicComponent(Topic3_4)} />
+        <Route path="/level3/Topic3_5" element={wrapTopicComponent(Topic3_5)} />
+        <Route path="/level3/Topic3_6" element={wrapTopicComponent(Topic3_6)} />
+        <Route path="/level3/Topic3_7" element={wrapTopicComponent(Topic3_7)} />
+        <Route path="/level3/Topic3_8" element={wrapTopicComponent(Topic3_8)} />
       </Routes>
     </>
   );
